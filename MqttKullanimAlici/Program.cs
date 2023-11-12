467e5ed1201a5be9c2be134953a470702576b5e5
MEHMET <MEHMET@DESKTOP-H6NC22D>
2023-11-12 17:43:17 +0300

    Add project files.

---
+++ MqttKullanimAlici/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using uPLibrary.Networking.M2Mqtt;
+using uPLibrary.Networking.M2Mqtt.Messages;
+
+namespace MqttKullanimAlici
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MqttClient mqttClient = new MqttClient("broker.hivemq.com");
+            mqttClient.MqttMsgPublishReceived += client_recievedMessage;
+
+            string clientId = Guid.NewGuid().ToString();
+            mqttClient.Connect(clientId);
+            Console.WriteLine("Abone Olundu: test/deneme2");
+
+            mqttClient.Subscribe(new String[] { "test/deneme2" }, new byte[] { MqttMsgBase.QOS_LEVEL_AT_LEAST_ONCE });
+        }
+        static void client_recievedMessage(object sender, MqttMsgPublishEventArgs e)
+        {
+            var message = System.Text.Encoding.Default.GetString(e.Message);
+            System.Console.WriteLine("Alınan Mesaj: " + message);
+        }
+    }
+}
---
+++ MqttKullanimOrnekNet/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using uPLibrary.Networking.M2Mqtt;
+using uPLibrary.Networking.M2Mqtt.Messages;
+
+namespace MqttKullanimOrnekNet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MqttClient mqttClient = new MqttClient("broker.hivemq.com");
+            string clientId = Guid.NewGuid().ToString();
+
+            mqttClient.Connect(clientId);
+            Console.WriteLine("Abone Olundu: test/deneme2");
+            while (true)
+            {
+                Console.WriteLine("Mesajı girin: ");
+                string message = Console.ReadLine();
+                string Topic = "test/deneme2";
+                mqttClient.Publish(Topic, Encoding.UTF8.GetBytes(message), MqttMsgBase.QOS_LEVEL_EXACTLY_ONCE, true);
+            }
+         
+        }
+    }
+}
